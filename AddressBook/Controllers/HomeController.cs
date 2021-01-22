f67b78a7a357b5bd1cd0d3bace30888ed9a412df
pallvi-grover <pallvi.grover@hcl.com>
2021-01-21 16:36:57 -0800

    New Views added

--- AddressBook/Controllers/HomeController.cs
+++ AddressBook/Controllers/HomeController.cs
@@ -21,5 +21,36 @@ namespace AddressBook.Controllers
              return View(employees);
             //return View();
         }
+
+        public ActionResult Edit(int id)
+        {
+            var employee = (db.ContactsInfos.Where(y => y.ID == id).Select(x => new { Contacts = x, x.phoneNumbers })).ToList();
+            return View(employee[0].Contacts);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                var employee = db.ContactsInfos.Single(m => m.ID == id);
+                if (TryUpdateModel(employee))
+                {
+                    //To Do:- database code
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                return View(employee);
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
     }
 }
